ddea7ea3666ca59e5bbb0efd7b24185a6d989e42
Mallika <kmallika.malli@gmail.com>
2023-10-05 10:50:15 +0530

    changes in ssm parameters and tf-module-db

---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "name" {
+  default = "DOC_DB"
+}
+variable "env" {}
+variable "allow_db_cidr" {}
+variable "vpc_id" {}
+variable "subnets" {}
+variable "tags" {}
+variable "port_no" {
+  default = "27017"
+}
+variable "engine_version" {}
+variable "kms_arn" {}
